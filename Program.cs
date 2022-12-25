5ffa72797d05b123050271a6f6aa6be332e81798
esseucar <79807819+esseucar@users.noreply.github.com>
2022-12-25 19:50:35 +0300

    Add files via upload
    Bir ders için yapılan iki ara sınav ve bir genel sınav sonucu değerlendirilerek geçme notları hesaplanacaktır. Bu dersin geçme notu 1. ara sınav notunun %20’si, 2. ara sınav notunun %30’u ve genel sınav notunun %50’si toplanarak hesaplanmaktadır. Geçme notunu hesaplayan ve sonucu ekrana yazdıran programı yazın.

--- Program.cs
+++ Program.cs
@@ -1,39 +1,29 @@
-//Klavyeden girilen A ve B gibi iki sayıyı,  sadece toplama ve çıkarma işlemlerini kullanarak 
-//(direkt çarpma işlemi kullanmadan) çarpma işlemini yapan ve bunun sonucunu ekrana, 
-//ekranın geri plan rengi beyaz ve ekrana yazılan mesajın rengi kırmızı olacak 
-//şekilde yazan programı yazınız.
 
+// Bir ders için yapılan iki ara sınav ve bir genel sınav sonucu değerlendirilerek geçme notları hesaplanacaktır.
+//Bu dersin geçme notu 1. ara sınav notunun %20’si, 2. ara sınav notunun %30’u ve genel sınav notunun %50’si toplanarak 
+//hesaplanmaktadır. Geçme notunu hesaplayan ve sonucu ekrana yazdıran programı yazın.
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Multiply
-{
+namespace PassingGrade
+{   
     class Program
     {
         static void Main(string[] args)
         {
-            
-            //Read num A and B nums
-            Console.WriteLine("Enter A Number");
-            int A = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter B Number");
-            int B = int.Parse(Console.ReadLine());
-            int res = 0;
-            for(int i=1; i <= B; i++)
-            {
-                res = res+A;
-            }
+            // Declare variables for midterm grades and final exam grade
+            Console.WriteLine("midterm1");
+            double midterm1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("midterm2");
+            double midterm2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("finalExam");
+            double finalExam = double.Parse(Console.ReadLine());
 
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(res);
+            // Calculate passing grade
+            double passingGrade = (midterm1 * 0.2) + (midterm2 * 0.3) + (finalExam * 0.5);
+
+            // Print passing grade to the console
+            Console.WriteLine("The passing grade is: " + passingGrade);
             Console.ReadKey();
 
-             
         }
     }
 }
